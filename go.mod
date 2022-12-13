bdeace2c92d276b2231c5ac60b45386f502eb0e8
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-12-13 11:59:41 -0300

    :sparkles: feat: implements function that print hello World

---
+++ go.mod
@@ -0,0 +1,3 @@
+module curso_udemy_-_golang_do_zero
+
+go 1.19
---
+++ introducao/ola_mundo.go
@@ -0,0 +1,8 @@
+package main //Arquivo executavel
+import "fmt"
+
+func main() {
+	fmt.Println("Olá Mundo!")
+}
+
+// go run introducao/ola_mundo.go
