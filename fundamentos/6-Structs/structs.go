dffc1f0c9abf7de5a196f8ca1fbd250279e23fe3
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-12-19 13:16:19 -0300

    feat: structs

--- fundamentos/6-Structs/structs.go
+++ fundamentos/6-Structs/structs.go
@@ -1,1 +1,34 @@
-package __Structs
+package main
+
+import "fmt"
+
+// struct entra no lugar da class
+type usuario struct {
+	nome     string
+	idade    uint8
+	endereco endereco
+}
+
+type endereco struct {
+	logradouro string
+	numero     uint8
+}
+
+func main() {
+	fmt.Println("Arquivo structs")
+
+	// forma 1
+	var u usuario
+	u.nome = "Davi"
+	u.idade = 21
+	fmt.Println(u)
+
+	enderecoExemplo := endereco{"Rua dos Bobos", 0}
+
+	//forma 2
+	usuario2 := usuario{"Davi", 21, enderecoExemplo}
+	fmt.Println(usuario2)
+
+	usuario3 := usuario{idade: 22} //preenche o campo especificado pela chave
+	fmt.Println(usuario3)
+}
