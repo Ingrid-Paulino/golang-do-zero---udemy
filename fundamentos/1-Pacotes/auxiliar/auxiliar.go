f8fe7645714753468aeb74183f419ab83ef6397a
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-12-19 13:19:45 -0300

    feat: files auxiliar

--- fundamentos/1-Pacotes/auxiliar/auxiliar.go
+++ fundamentos/1-Pacotes/auxiliar/auxiliar.go
@@ -1,1 +1,8 @@
 package auxiliar
+
+import "fmt"
+
+func Escrever() { //Nome das funçoes precisa ser declarado a primeira letra em maiusculo - isso indica que a funçao é publica e não privada
+	fmt.Println("Escrevendo do pacote auxiliar")
+	escrever2() //Funçoes de pacotes diferentes privadas, eu consigo chamae em arquivos diferentes mas que esteja no mesmo pacote
+}
