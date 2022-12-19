5a0ee44bc161e7d0f65f1ad31231571c833823da
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-12-19 13:21:20 -0300

    feat: missing files

---
+++ fundamentos/1-Pacotes/go.mod
@@ -0,0 +1,7 @@
+module modulo
+
+//esse arquivo é um modulo -aqui fica um conjunto de pacotes que compoe o projeto
+//Para criar esse arquivo rode: go mod init nomeDoModulo(modulo)
+go 1.19
+
+require github.com/badoux/checkmail v1.2.1 // indirect
--- fundamentos/1-Pacotes/main.go
+++ fundamentos/1-Pacotes/main.go
@@ -1,1 +1,14 @@
-package __Pacotes
+package main
+
+import (
+	"fmt"                         //pacote interno
+	"github.com/badoux/checkmail" // pacote externo
+	"modulo/auxiliar"             //pacote interno
+)
+
+func main() {
+	fmt.Println("Escrevendo do arquivo main")
+	auxiliar.Escrever()
+	err := checkmail.ValidateFormat("email@gmail.com") //retorna nil/null se estiver tudo certp
+	fmt.Println(err)
+}
