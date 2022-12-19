3400205145bf66dd150f877d08f4b2ed4d5a1e16
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-12-19 13:12:20 -0300

    feat: maps

--- fundamentos/10-maps/maps.go
+++ fundamentos/10-maps/maps.go
@@ -1,1 +1,35 @@
-package _0_maps
+package main
+
+import "fmt"
+
+func main() {
+	// map cria objetos
+	//map[tipo das chaves]tipo dos valores
+	usuario := map[string]string{
+		"nome":      "Pedro",
+		"sobrenome": "Silva",
+	}
+
+	fmt.Println(usuario["nome"]) // acessa as chaves
+
+	usuario2 := map[string]map[string]string{
+		"nome": {
+			"primeiro": "João",
+			"ultimo":   "Pedro",
+		},
+		"curso": {
+			"nome":   "Engenharia",
+			"campus": "Campus 1",
+		},
+	}
+
+	fmt.Println(usuario2)
+	delete(usuario2, "nome") // deleta uma chave
+	fmt.Println(usuario2)
+
+	usuario2["signo"] = map[string]string{ // adiciona uma chave no map
+		"nome": "Gêmeos",
+	}
+
+	fmt.Println(usuario2)
+}
