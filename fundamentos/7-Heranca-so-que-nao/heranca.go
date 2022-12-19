cda20c60118d4361e0284956cb8ba22aac833423
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-12-19 13:16:42 -0300

    feat: heranca-so-que-nao

--- fundamentos/7-Heranca-so-que-nao/heranca.go
+++ fundamentos/7-Heranca-so-que-nao/heranca.go
@@ -1,1 +1,28 @@
-package __Heranca
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+	idade     uint8
+	altura    uint8
+}
+
+type estudante struct {
+	pessoa    //heranca
+	curso     string
+	faculdade string
+}
+
+func main() {
+	fmt.Println("Herança")
+
+	p1 := pessoa{"João", "Pedro", 20, 178}
+	fmt.Println(p1)
+
+	e1 := estudante{p1, "Engenharia", "USP"}
+	fmt.Println(e1)
+	fmt.Println(e1.nome)
+	fmt.Println(e1.altura)
+}
