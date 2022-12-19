fb63e8a7043f3367cbd7e167a58be2e63f06df56
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-12-19 13:16:59 -0300

    feat: ponteiros

--- fundamentos/8-Ponteiros/ponteiros.go
+++ fundamentos/8-Ponteiros/ponteiros.go
@@ -1,1 +1,25 @@
-package __Ponteiros
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 int = 10
+	var variavel2 int = variavel1 // copia
+
+	fmt.Println(variavel1, variavel2)
+
+	variavel2++
+	fmt.Println(variavel1, variavel2)
+
+	// PONTEIRO É UMA REFERÊNCIA DE MEMÓRIA
+	var variavel3 int // valor padrao é 0
+	var ponteiro *int // valor padrao é nil // no ponteiro faz referencia do endereço de memoria
+
+	variavel3 = 100
+	ponteiro = &variavel3 // estou jogando o endereco de memoria "&variavel3" dentro do "ponteiro"
+	fmt.Println(variavel3, ponteiro)
+
+	variavel3 = 150
+	fmt.Println(variavel3, *ponteiro) // para eu ver o valor do ponteiro eu coloco o *
+
+}
